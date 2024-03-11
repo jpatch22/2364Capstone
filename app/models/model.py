7038a5660310f2885c0f21b327dd20bc16bbe885
Jack Patchell <jackpatchell222@gmail.com>
2024-03-10 20:19:31 -0700

    scaffolding

---
+++ app/models/model.py
@@ -0,0 +1,7 @@
+class Model:
+    def __init__(self):
+        pass
+
+    def supply_annotations(self, image_list):
+        pass
+
---
+++ app/models/test_model.py
@@ -0,0 +1,9 @@
+from model import Model
+
+class Test_Model(Model):
+    def __init__(self):
+        super().__init__()
+
+    def supply_annotations(self, image_list):
+        super().supply_annotations(image_list)
+
